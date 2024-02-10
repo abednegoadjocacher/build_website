318040c09141df347b1fac267bb0c494cf45355d
Adjocacher Abednego <abednegoadjocacher@gmail.com>
2024-02-10 05:19:20 -1200

    Character and ASCII value

---
+++ C++/char.cpp
@@ -0,0 +1,14 @@
+/**
+ * this is to print a character and it numerical valus
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch='a';
+//  std::cout<<ch<<endl;
+   cout<<ch<<endl;
+   //typecasting it to print the ASCII value
+   cout<<(int)ch<<endl;
+    return 0;
+}
